41781f6357e060353d6760d0c8e70a971c3199dc
akalanka <shenalakalanka513@gmail.com>
2024-05-27 00:12:28 +0530

    NBAP-2 FO migration to message type request

--- src/app/app-routing/app.routes.ts
+++ src/app/app-routing/app.routes.ts
@@ -1,15 +1,14 @@
 import { Routes } from '@angular/router';
-import { AppSignupComponent } from '../app-widgets/auth-widgets/app-signup/app-signup.component';
-import { AppSigninComponent } from '../app-widgets/auth-widgets/app-signin/app-signin.component';
-import { DashboardComponent } from '../app-layout/dashboard/dashboard/dashboard.component';
+
 import { dashboardroutes } from './dashboard.routes';
 import { AuthActivateRouteGuard } from '../app-services/routegurd-services/auth.routegurd';
-import { UnauthorizedAccessComponent } from '../app-widgets/common-widgets/unauthorized-access/unauthorized-access.component';
+import { AppSignupComponent } from '../app-widgets/auth-widgets/app-signup/app-signup.component';
+import { AppSigninComponent } from '../app-widgets/auth-widgets/app-signin/app-signin.component';
 
 export const routes: Routes = [
     {path: 'register', component:AppSignupComponent},
     {path: 'login', component:AppSigninComponent},
     {path: 'secure', redirectTo: 'secure/dashboard', pathMatch: 'full'},
-    {path: 'secure',  component: DashboardComponent, children: dashboardroutes, canActivate: [AuthActivateRouteGuard]},
-    {path: 'unauthorized', component: UnauthorizedAccessComponent}
+    // {path: 'secure',  component: DashboardComponent, children: dashboardroutes, canActivate: [AuthActivateRouteGuard]},
+    // {path: 'unauthorized', component: UnauthorizedAccessComponent}
 ];
--- src/app/app-widgets/auth-widgets/app-signup/app-signup.component.ts
+++ src/app/app-widgets/auth-widgets/app-signup/app-signup.component.ts
@@ -1,66 +1,12 @@
-import { Component, inject } from '@angular/core';
-import { UserStatus, UserTypes } from '../../../app-constants/enum/user.enum';
-import { CommonModule } from '@angular/common';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { UserSignUpModel } from '../../../app-constants/interface/user.interface';
-import { Router } from '@angular/router';
-import Swal from 'sweetalert2';
-import { HotToastService } from '@ngneat/hot-toast';
-import { AuthAPIService } from '../../../app-services/api-services/auth-api.service';
-import { BaseWidgetDirective } from '../../../app-utils/base-widget/base-widget.directive';
-
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-app-signup',
   standalone: true,
-  imports: [ CommonModule, ReactiveFormsModule],
-  providers: [Router],
+  imports: [],
   templateUrl: './app-signup.component.html',
-  styleUrl: './app-signup.component.scss', 
+  styleUrl: './app-signup.component.scss'
 })
-
-
-export class AppSignupComponent extends BaseWidgetDirective{
-
-  
-
-  public signUpForm = new FormGroup({
-    fname: new FormControl('', [Validators.required, Validators.pattern("[a-zA-Z ]*")]),
-    lname: new FormControl('', [Validators.required, Validators.pattern("[a-zA-Z ]*")]),
-    email: new FormControl('', [Validators.required, Validators.pattern("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$")]),
-    password: new FormControl('', [Validators.required, Validators.pattern('^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[!@#$%^&*()_+}{":;\'?/>.<,])(?!.*\\s).{8,16}$')]),
-    confirmPassword: new FormControl('', [Validators.required, Validators.pattern('^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[!@#$%^&*()_+}{":;\'?/>.<,])(?!.*\\s).{8,16}$')])
-  });
-
-  constructor(private router: Router,  private authService: AuthAPIService){
-    super(inject(HotToastService));
-  }
-
-  public onSubmit(event: Event){
-    console.log("cleicked")
-    event.preventDefault();
-    if( this.validator(this.signUpForm)){
-       const formData: UserSignUpModel = {
-        fname: this.signUpForm.value.fname ?? '',
-        lname: this.signUpForm.value.lname ?? '',
-        email: this.signUpForm.value.email ?? '',
-        password: this.signUpForm.value.password ?? '',
-        
-       };
-       this.authService.signUpUser(formData);
-    }
-    // console.log(this.signUpForm.value);
-    // this.formData.createdAt = new Date();
-    // this.formData.status = UserStatus.pending;
-    // console.log(this.formData);
-    // this.router.navigate(['/profiling']);
-
-  }
-
-  public handleLoginClick(){
-    this.router.navigate(['/login']);
-   }
-
-   
+export class AppSignupComponent {
 
 }
