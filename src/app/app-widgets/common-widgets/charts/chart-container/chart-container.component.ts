cd3c0d7e580e59064ac0bad5ac6a8b92f6b72bbe
akalanka <shenalakalanka513@gmail.com>
2024-06-16 11:08:06 +0530

    NBAP-3 FO apply new user interface

---
+++ src/app/app-widgets/common-widgets/charts/chart-container/chart-container.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chart-container',
+  standalone: true,
+  imports: [],
+  templateUrl: './chart-container.component.html',
+  styleUrl: './chart-container.component.scss'
+})
+export class ChartContainerComponent {
+
+}
